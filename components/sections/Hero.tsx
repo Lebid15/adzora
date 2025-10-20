330d0608596c59695f0cb8110adb5e168c530df4
Abdulrahman <aalkhalil@gmail.com>
2025-10-20 13:58:55 +0300

    test: add emoji to hero section

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -71,7 +71,7 @@ export default function Hero() {
             animate={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.4, duration: 0.8 }}
           >
-            نصنع محتوى بصري إبداعي يحكي قصتك بطريقة مميزة
+            نصنع محتوى بصري إبداعي يحكي قصتك بطريقة مميزة ✨
           </motion.p>
 
           <motion.p
